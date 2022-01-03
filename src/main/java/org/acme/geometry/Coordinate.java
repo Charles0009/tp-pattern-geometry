4d9f87792de09babe23afca6e0108347b770bb8a
charles0008 <charles.laverdure@numericable.fr>
2022-01-03 15:45:51 +0100

    commit for question 0.2

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -5,6 +5,15 @@ public class Coordinate {
     private double x = 0.0;
     private double y = 0.0; 
 
+    public Coordinate(){
+
+    }
+
+    public Coordinate(double x, double y){
+        this.x = x;
+        this.y = y; 
+    }
+
     public double getX() {
         return x; 
     }
